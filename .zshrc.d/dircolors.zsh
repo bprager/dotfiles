7a47ec14e1ef21450998777a79c0b0f6caf0509a
Bernd Prager <bernd@prager.ws>
2024-11-21 01:47:48 +0000

    git gh script location and some ls color support

--- .zshrc.d/dircolors.zsh
+++ .zshrc.d/dircolors.zsh
@@ -1,6 +1,7 @@
 # enable color support of ls and add some handy aliases"
 if [ -x /usr/bin/dircolors ]; then
   test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -n)"
+  LS_COLORS+=':ow=01;34'
   alias ls='ls --color=auto'
   alias grep='grep --color=auto'
   alias fgrep='fgrep --color=auto'
