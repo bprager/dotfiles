6904684da2020402466c77d456963168f5f9d982
Bernd Prager <bernd@prager.ws>
2025-01-24 12:33:50 -0800

    update cuda for pytorch

--- .zshrc.d/cuda.zsh
+++ .zshrc.d/cuda.zsh
@@ -4,5 +4,7 @@ if lsmod | grep -q "nvidia"; then
     export CUDA_HOME=/usr/local/cuda
     export PATH=$CUDA_HOME/bin:$PATH
     export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+    export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+    export TOKENIZERS_PARALLELISM=false
 fi
 
