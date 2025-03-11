35bd225836eeb2ed7dc5f534722b38a9c725fc47
Bernd Prager <bernd@prager.ws>
2025-03-11 09:26:56 -0700

    add check for 'virtualenv-init'

--- .zshrc.d/pyenv.zsh
+++ .zshrc.d/pyenv.zsh
@@ -1,5 +1,12 @@
-# Load pyenv-virtualenv automatically
+# Load pyenv-virtualenv automatically if available
 export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+if [[ -d $PYENV_ROOT/bin ]]; then
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    # Check if virtualenv-init is available before running it
+    if command -v pyenv virtualenv-init &> /dev/null; then
+        eval "$(pyenv virtualenv-init -)"
+    fi
+fi
+
