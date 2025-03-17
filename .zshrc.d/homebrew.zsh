dc73b503da0936427ed863e94b403941970e43b3
Bernd Prager <bernd@prager.ws>
2025-03-17 13:35:13 -0700

    add alternative location

--- .zshrc.d/homebrew.zsh
+++ .zshrc.d/homebrew.zsh
@@ -1,6 +1,12 @@
 # homebrew program is installed
 if [ -d "$HOME/homebrew" ]; then
   export PATH="$HOME/homebrew/bin/:$PATH"
-  # auto update every 24 hours
+elif [ -d "/opt/homebrew" ]; then
+  export PATH="/opt/homebrew/bin/:$PATH"
+fi
+
+# auto update every 24 hours if homebrew is installed
+if [ -d "$HOME/homebrew" ] || [ -d "/opt/homebrew" ]; then
   export HOMEBREW_AUTO_UPDATE_SECS="86400"
 fi
+
