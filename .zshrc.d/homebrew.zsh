792bdb8dafc4ab00315b189b133599c70074f03f
Bernd Prager <bernd@prager.ws>
2025-03-17 12:54:24 -0700

    homebrew path

--- .zshrc.d/homebrew.zsh
+++ .zshrc.d/homebrew.zsh
@@ -1,5 +1,6 @@
 # homebrew program is installed
 if which brew >/dev/null 2>&1; then
   # auto update every 24 hours
+  export PATH="$HOME/homebrew/bin/:$PATH"
   export HOMEBREW_AUTO_UPDATE_SECS="86400"
 fi
