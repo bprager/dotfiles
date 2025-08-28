64013911cf0198542df7d7086cde674a5a1e7d5f
Bernd Prager <bernd@prager.ws>
2025-08-28 13:33:12 -0700

    add never logged in message

--- .zshrc.d/lastlog.zsh
+++ .zshrc.d/lastlog.zsh
@@ -1,5 +1,14 @@
 # checks if lastlog exists, and if not, creates a fallback alias
 
 if ! command -v lastlog >/dev/null 2>&1; then
-  alias lastlog='last -F | awk '\''!seen[$1]++ && $1 != "reboot" && $1 != "wtmp" && $1 != "" {print $1, $3, $4, $5, $6, $7, $8, $9}'\'''
+  alias lastlog='
+  getent passwd | awk -F: '\''$3 >= 1000 && $3 != 65534 {print $1}'\'' | while read user; do
+    login_info=$(last -F -n 1 "$user" | grep -vE "^(reboot|wtmp|$)" | head -n 1)
+    if [ -z "$login_info" ]; then
+      printf "%-15s **Never logged in**\n" "$user"
+    else
+      printf "%s\n" "$login_info"
+    fi
+  done
+  '
 fi
